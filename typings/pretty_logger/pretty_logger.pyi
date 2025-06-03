5ef7181a95d747e2c7fa075de2ca51c8e2b22e60
Matthieu Liger <matthieu.liger@gmail.com>
2025-06-02 22:13:54 -0700

    typings again

--- typings/pretty_logger/pretty_logger.pyi
+++ typings/pretty_logger/pretty_logger.pyi
@@ -1,5 +1,5 @@
 import logging
-from _typeshed import Incomplete as Incomplete
+from _typeshed import Incomplete
 from pathlib import Path
 
 level_styles: Incomplete
@@ -15,11 +15,26 @@ class WrappedColoredFormatter(logging.Formatter):
     base_formatter: Incomplete
     width: Incomplete
     def __init__(self, base_formatter, width: int = 240) -> None: ...
-    def format(self, record) -> None: ...
+    def format(self, record): ...
 
 class ClassLogger(logging.Logger):
     EMPTY_CLASS: str
 
 def get_git_root() -> str | None: ...
-def configure_pretty_logging(level: str | int = ..., log_file: str | Path = ..., console: bool = True, width: int = 150, silenced_modules: list[str] = ['pymongo', 'motor', 'httpcore', 'httpx']): ...
-def get_module_logger(name: str, level=...) -> logging.Logger: ...
+def configure_pretty_logging(
+    level: str | int = ...,
+    log_file: str | Path = ...,
+    console: bool = True,
+    width: int = 150,
+    silenced_modules: list[str] = [
+        "pymongo",
+        "motor",
+        "httpcore",
+        "httpx",
+        "pdfminer",
+        "python_multipart",
+    ],
+): ...
+def get_module_logger(
+    name: str, level=..., width: int = 120
+) -> logging.Logger: ...
